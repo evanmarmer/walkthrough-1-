d266dcd64dec3714e5f3685376fa71ed16bc682e
Evan Marmer <evanmarmer@gmail.com>
2023-11-28 11:29:42 -0700

    initial commit, strings file done

---
+++ strings.js
@@ -0,0 +1,11 @@
+const hello = "hello world"
+let stringWithSingleQuotes = 'single quotes work too'
+console.log(hello[0])
+
+const ball = 'ball'
+const templateString = `foot${ball}`
+console.log(templateString)
+
+let yourName = 'evan'
+console.log(`hi, ${yourName}`)
+console.log('hi, ' +  yourName)
